2cc01be02790aad82c06f515ad6a241ef41dff72
Luong Van Dai <105350944+HE151191@users.noreply.github.com>
2022-07-07 02:19:54 +0700

    up
    register

---
+++ Project_PRN211_TEAM7/Project_PRN211_TEAM7/Controllers/UserController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Project_PRN211_TEAM7.Models;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Project_PRN211_TEAM7.Controllers
+{
+    public class UserController : Controller
+    {
+        PROJECT_PRN211_SHOES_APPContext context = new PROJECT_PRN211_SHOES_APPContext();
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Register(User acc, string re_password)
+        {
+            if(acc.Password == null || re_password == null || acc.UserName == null || acc.Address == null || acc.Phone == null)
+            {
+                return View();
+            }
+            if (!acc.Password.Equals(re_password))
+            {
+                ViewBag.password = "Confirmation password does not match";
+                return View();
+            }
+            Regex re = new Regex("[0-9]");
+            if (acc.Phone.Length > 8)
+            {
+                for (int i = 0; i < acc.Phone.Length; i++)
+                {
+                    if (!re.IsMatch(acc.Phone[i] + ""))
+                    {
+                        ViewBag.phone = "Please enter valid phone number";
+                        return View();
+                    }
+                }
+            }
+            else
+            {
+                ViewBag.phone = "Please enter valid phone number > 8";
+                return View();
+            }
+            if (ModelState.IsValid)
+            {
+                var user = context.Users.FirstOrDefault(u => u.UserName.Equals(acc.UserName));
+                if (user != null)
+                {
+                    ViewBag.user = "This UserName was exist!";
+                    return View();
+                }
+                else
+                {
+                    acc.Role = "customer";
+                    context.Users.Add(acc);
+                    context.SaveChanges();
+                    return Redirect("Home/Index");
+                }
+
+            }
+            else
+            {
+                return RedirectToAction("Register");
+            }
+        }
+    }
+}
--- Project_PRN211_TEAM7/Project_PRN211_TEAM7/Models/User.cs
+++ Project_PRN211_TEAM7/Project_PRN211_TEAM7/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -13,11 +14,25 @@ namespace Project_PRN211_TEAM7.Models
         }
 
         public int UserId { get; set; }
+        [Required (AllowEmptyStrings =false, ErrorMessage = "Input your UserName")]
         public string UserName { get; set; }
+        [Required (AllowEmptyStrings =false, ErrorMessage = "Input your Password")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+        [Required (AllowEmptyStrings =false, ErrorMessage = "Input your Address")]
         public string Address { get; set; }
+        [Required (AllowEmptyStrings =false, ErrorMessage = "Input your Phone")]
         public string Phone { get; set; }
         public string Role { get; set; }
+
+        public User(string userName, string password, string address, string phone, string role)
+        {
+            UserName = userName;
+            Password = password;
+            Address = address;
+            Phone = phone;
+            Role = role;
+        }
 
         public virtual ICollection<Oder> Oders { get; set; }
     }
