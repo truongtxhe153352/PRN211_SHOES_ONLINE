4ebc92d657985d20403e5cfb87fb059da2cfce87
truongtxhe153352 <truongtxhe153352@fpt.edu.vn>
2022-07-08 00:42:26 +0700

    Update2

--- Project_PRN211_TEAM7/Project_PRN211_TEAM7/Controllers/HomeController.cs
+++ Project_PRN211_TEAM7/Project_PRN211_TEAM7/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using System.Security.Cryptography;
 using System.Text;
-using Project_PRN211_TEAM7.Logic;
 using Microsoft.AspNetCore.Http;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Authorization;
@@ -24,141 +23,10 @@ namespace Project_PRN211_TEAM7.Controllers
             return View(db.Products.ToList()) ;
         }
 
-        public IActionResult Shop(int Id, int Page)
-        {
-            // lay danh sach category
-            BrandManager brandManager = new BrandManager();
-            ViewBag.Brand = brandManager.GetAllBrand();
-            // lay cac san pham trong brand yeu cau
-            if (Page <= 0) 
-            {
-                Page = 1; 
-            }
-            int PageSize = 3;
-            ProductManager productManager = new ProductManager();
-            List<Product> products = productManager.GetProduct(Id, (Page - 1) * PageSize + 1, PageSize);
+        
 
-            // Lay du lieu hien thi thanh phan trang
-            int TotalProduct = productManager.GetNumberOfProducts(Id);
-            int TotalPage = TotalProduct / PageSize;
-            if (TotalProduct % PageSize != 0)
-            {
-                TotalPage += 2;
-            }
-
-            ViewData["TotalPage"] = TotalPage;
-            ViewData["CurrentPage"] = Page;
-            ViewData["CurrentBra"] = Id;
-
-            return View(products);
-        }
-
-        public ActionResult Login()
-        {
-            ViewBag.Message = HttpContext.Session.GetString("username");
-
-            ViewBag.Error = "";
-
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Login(string username, string password)
-        {
-            try
-            {
-                ViewBag.Error = "";
-
-                // kiem tra trong co so du lieu co username va password chua
-                User user = db.Users.Where(m => m.UserName == username).FirstOrDefault();
-                if (user != null)
-                {
-                    if (user.Password.Equals(password))
-                    {
-                        HttpContext.Session.SetString("username", username);
-                        return RedirectToAction("Index");
-                    }
-                    else
-                    {
-                        ViewBag.Error = "Password not true!";
-                    }
-                }
-                else
-                {
-                    ViewBag.Error = "Account " + username + " Not exist!";
-                }
-
-            }
-            catch (System.Exception e)
-            {
-
-                System.Console.WriteLine(e);
-            }
-            return View("Login");
-        }
-
-        [HttpPost]
-        public ActionResult Logout()
-        {
-            HttpContext.Session.Remove("username");
-            return RedirectToAction("Index");
-        }
-        public IActionResult Register()
-        {
-            return View();
-        }
-        [HttpPost]
-        [AllowAnonymous]
-        public IActionResult Register(User acc, string re_password)
-        {
-            if (acc.Password == null || re_password == null || acc.UserName == null || acc.Address == null || acc.Phone == null)
-            {
-                return View();
-            }
-            if (!acc.Password.Equals(re_password))
-            {
-                ViewBag.password = "Confirmation password does not match";
-                return View();
-            }
-            Regex re = new Regex("[0-9]");
-            if (acc.Phone.Length > 8)
-            {
-                for (int i = 0; i < acc.Phone.Length; i++)
-                {
-                    if (!re.IsMatch(acc.Phone[i] + ""))
-                    {
-                        ViewBag.phone = "Please enter valid phone number";
-                        return View();
-                    }
-                }
-            }
-            else
-            {
-                ViewBag.phone = "Please enter valid phone number > 8";
-                return View();
-            }
-            if (ModelState.IsValid)
-            {
-                var user = db.Users.FirstOrDefault(u => u.UserName.Equals(acc.UserName));
-                if (user != null)
-                {
-                    ViewBag.user = "This UserName was exist!";
-                    return View();
-                }
-                else
-                {
-                    acc.Role = "customer";
-                    db.Users.Add(acc);
-                    db.SaveChanges();
-                    return Redirect("Home/Index");
-                }
-
-            }
-            else
-            {
-                return RedirectToAction("Register");
-            }
-        }
+       
+        
 
     }
 }
---
+++ Project_PRN211_TEAM7/Project_PRN211_TEAM7/Controllers/UserController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Project_PRN211_TEAM7.Models;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Authorization; 
+
+namespace Project_PRN211_TEAM7.Controllers
+{
+    public class UserController : Controller
+    {
+        PROJECT_PRN211_SHOES_APPContext db = new PROJECT_PRN211_SHOES_APPContext();
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Register(User acc, string re_password)
+        {
+            if (acc.Password == null || re_password == null || acc.UserName == null || acc.Address == null || acc.Phone == null)
+            {
+                return View();
+            }
+            if (!acc.Password.Equals(re_password))
+            {
+                ViewBag.password = "Confirmation password does not match";
+                return View();
+            }
+            Regex re = new Regex("[0-9]");
+            if (acc.Phone.Length > 8)
+            {
+                for (int i = 0; i < acc.Phone.Length; i++)
+                {
+                    if (!re.IsMatch(acc.Phone[i] + ""))
+                    {
+                        ViewBag.phone = "Please enter valid phone number";
+                        return View();
+                    }
+                }
+            }
+            else
+            {
+                ViewBag.phone = "Please enter valid phone number > 8";
+                return View();
+            }
+            if (ModelState.IsValid)
+            {
+                var user = db.Users.FirstOrDefault(u => u.UserName.Equals(acc.UserName));
+                if (user != null)
+                {
+                    ViewBag.user = "This UserName was exist!";
+                    return View();
+                }
+                else
+                {
+                    acc.Role = "customer";
+                    db.Users.Add(acc);
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "Home");
+                }
+
+            }
+            else
+            {
+                return RedirectToAction("Register");
+            }
+        }
+
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = HttpContext.Session.GetString("username");
+
+            ViewBag.Error = "";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            try
+            {
+                ViewBag.Error = "";
+
+                // kiem tra trong co so du lieu co username va password chua
+                User user = db.Users.Where(m => m.UserName == username).FirstOrDefault();
+                if (user != null)
+                {
+                    if (user.Password.Equals(password))
+                    {
+                        HttpContext.Session.SetString("username", username);
+                        return RedirectToAction("Index","Home");
+                    }
+                    else
+                    {
+                        ViewBag.Error = "Password not true!";
+                    }
+                }
+                else
+                {
+                    ViewBag.Error = "Account " + username + " Not exist!";
+                }
+
+            }
+            catch (System.Exception e)
+            {
+
+                System.Console.WriteLine(e);
+            }
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            HttpContext.Session.Remove("username");
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
