760323ccf92ceceb1e7406ce8fbcefc621e30f12
Luong Van Dai <105350944+HE151191@users.noreply.github.com>
2022-07-07 16:50:16 +0700

    up
    HomePage

--- Project_PRN211_TEAM7/Project_PRN211_TEAM7/Controllers/HomeController.cs
+++ Project_PRN211_TEAM7/Project_PRN211_TEAM7/Controllers/HomeController.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Project_PRN211_TEAM7.Models;
+using System.Linq;
 
 namespace Project_PRN211_TEAM7.Controllers
 {
     public class HomeController : Controller
     {
+        PROJECT_PRN211_SHOES_APPContext context = new PROJECT_PRN211_SHOES_APPContext();
         public IActionResult Index()
         {
-            return View();
+            ViewBag.Brand = context.Brands.ToList();
+            
+            return View(context.Products.ToList());
         }
     }
 }
