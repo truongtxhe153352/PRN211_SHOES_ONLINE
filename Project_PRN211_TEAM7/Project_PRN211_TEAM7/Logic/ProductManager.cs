013482c3efcb50224aa6252b60808a5a2e9b38bc
truongtxhe153352 <truongtxhe153352@fpt.edu.vn>
2022-07-07 22:41:28 +0700

    Login_logout_productList_paging_category

---
+++ Project_PRN211_TEAM7/Project_PRN211_TEAM7/Logic/ProductManager.cs
@@ -0,0 +1,41 @@
+﻿using Project_PRN211_TEAM7.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Project_PRN211_TEAM7.Logic
+{
+    public class ProductManager
+    {
+        public List<Product> GetProduct(int BrandId, int Offset, int Count)
+        {
+            using (var db = new PROJECT_PRN211_SHOES_APPContext())
+            {
+                if (BrandId == 0) // get all product
+                {
+                    return db.Products.Skip(Offset - 1).Take(Count).ToList();
+                }
+                else
+                {
+                    return db.Products.Where(x => x.BrandId == BrandId).Skip(Offset - 1).Take(Count).ToList();
+                }
+            }
+        }
+
+        public int GetNumberOfProducts(int BrandId)
+        {
+            using (var db = new PROJECT_PRN211_SHOES_APPContext())
+            {
+                if (BrandId == 0) // get all product
+                {
+                    return db.Products.Count();
+                }
+                else
+                {
+                    return db.Products.Where(x => x.BrandId == BrandId).Count();
+                }
+            }
+        }
+
+       
+    }
+}
---
+++ Project_PRN211_TEAM7/Project_PRN211_TEAM7/Models/Size.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Project_PRN211_TEAM7.Models
+{
+    public partial class Size
+    {
+        public int ProductId { get; set; }
+        public int Size1 { get; set; }
+        public int? Quantity { get; set; }
+
+        public virtual Product Product { get; set; }
+    }
+}
